05d7f6d256760d99fab16499b56548481c07c71b
Akshit Thakur <akshit2649@gmail.com>
2022-06-05 23:56:38 +0530

    Layout

--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
 const TourCard = () => {
-  return <Paper elevation={3}>TourCard</Paper>;
+  return (
+    <Grid item xs={3}>
+      <Paper elevation={3}>TourCard</Paper>
+    </Grid>
+  );
 };
 
 export default TourCard;
